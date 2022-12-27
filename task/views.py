f8e79a5615398018f4d637dadfd4033c06acaeef
DonGuillotine <infect3dlab@gmail.com>
2022-12-27 09:14:08 +0100

    A todo List App

---
+++ task/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import *
+from .forms import *
+
+
+# Create your views here.
+
+
+def index(request):
+    task = Task.objects.all()
+    form = TaskForm
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    context = {'task': task, 'form': form}
+    return render(request, 'task/list.html', context)
+
+
+def updateTask(request, pk):
+    task = Task.objects.get(id=pk)
+    form = TaskForm(instance=task)
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    context = {'form': form}
+    return render(request, 'task/update_task.html', context)
+
+
+def delete_task(request, pk):
+    item = Task.objects.get(id=pk)
+    if request.method == 'POST':
+        item.delete()
+        return redirect('/')
+    context = {'item': item}
+    return render(request, 'task/delete.html', context)
